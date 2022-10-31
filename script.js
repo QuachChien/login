eded52272438d00563bbc256ed16528b7904132a
CHIENQ <96031443+CHIENQ@users.noreply.github.com>
2022-10-31 23:38:03 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+const signUpBtn = document.querySelector(".blue-bgr button");
+
+signUpBtn.addEventListener("click",() => {
+    container.classList.toggle("change");
+}
+);
